b6c40525dfd0b68b4bad6d6aaccd1dce59104a55
eckzen <hackwebdev@gmail.com>
2021-03-15 23:45:18 +0800

    initial navbar

---
+++ components/Navbar.js
@@ -0,0 +1,7 @@
+export const Navbar = () => {
+  return (
+    <>
+      <nav className='flex items-center flex-wrap bg-green-300 p-3 '></nav>
+    </>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import { Navbar } from '../components/Navbar'
 
 export default function Home() {
   return (
@@ -7,6 +8,7 @@ export default function Home() {
         <title>Create Next App</title>
         <link rel='icon' href='/favicon.ico' />
       </Head>
+      <Navbar />
       <div>NextJS Responsive Navigation Bar</div>
     </div>
   )
