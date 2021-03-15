e8c2a80699e0afda3ea927688f80e01f84f4802a
eckzen <hackwebdev@gmail.com>
2021-03-15 23:49:48 +0800

    add menu for large screens

--- components/Navbar.js
+++ components/Navbar.js
@@ -3,7 +3,7 @@ import Link from 'next/link'
 export const Navbar = () => {
   return (
     <>
-      <nav className='flex items-center flex-wrap bg-green-300 p-3 '>
+      <nav className='flex items-center flex-wrap bg-green-400 p-3 '>
         <Link href='/'>
           <a className='inline-flex items-center p-2 mr-4 '>
             <svg
@@ -34,6 +34,30 @@ export const Navbar = () => {
             />
           </svg>
         </button>
+        <div className='hidden w-full lg:inline-flex lg:flex-grow lg:w-auto'>
+          <div className='lg:inline-flex lg:flex-row lg:ml-auto lg:w-auto w-full lg:items-center items-start  flex flex-col lg:h-auto'>
+            <Link href='/'>
+              <a className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center hover:bg-green-600 hover:text-white '>
+                Home
+              </a>
+            </Link>
+            <Link href='/'>
+              <a className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center hover:bg-green-600 hover:text-white'>
+                Services
+              </a>
+            </Link>
+            <Link href='/'>
+              <a className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center hover:bg-green-600 hover:text-white'>
+                About us
+              </a>
+            </Link>
+            <Link href='/'>
+              <a className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center hover:bg-green-600 hover:text-white'>
+                Contact us
+              </a>
+            </Link>
+          </div>
+        </div>
       </nav>
     </>
   )
